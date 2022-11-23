3e1480e837b1796e773096c8c764d94a1a8e0fb4
AHMED ABDELSALAM <ahmed_abdelsalam@epam.com>
2022-11-23 13:42:31 +0100

    add design structure for photo details component

--- src/app/photo-library/components/photo-details/photo-details.component.ts
+++ src/app/photo-library/components/photo-details/photo-details.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-photo-details',
   templateUrl: './photo-details.component.html',
-  styleUrls: ['./photo-details.component.sass']
+  styleUrls: ['./photo-details.component.scss']
 })
 export class PhotoDetailsComponent {
 
