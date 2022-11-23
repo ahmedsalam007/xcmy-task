d9c2ef12b413599e7f40894469fef05baa47bbfd
AHMED ABDELSALAM <ahmed_abdelsalam@epam.com>
2022-11-23 13:08:44 +0100

    add markup structure and style for photolist component

--- src/app/photo-library/components/photo-list/photo-list.component.ts
+++ src/app/photo-library/components/photo-list/photo-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.sass']
+  styleUrls: ['./photo-list.component.scss']
 })
 export class PhotoListComponent {
 
